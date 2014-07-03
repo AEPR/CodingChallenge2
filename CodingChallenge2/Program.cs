8fe7abdf975ef382eea10054cfb622f2e665f1c6
Alexander Read <alexander.read@softwire.com>
2014-07-03 11:11:42 +0100

    added robustness against incorrect file paths, and split UserInput into a new class

--- CodingChallenge2/Program.cs
+++ CodingChallenge2/Program.cs
@@ -7,31 +7,13 @@ using System.Threading.Tasks;
 namespace CodingChallenge2 {
     class Program {
 
-        private static int GetUsersFrequency() {
 
-            bool noValidAnswer = true;
-            int desiredFrequency = 0;
-            while (noValidAnswer) {
-                noValidAnswer = false;
-                Console.Write("Enter desired frequency to be matched: ");
-                try {
-                    desiredFrequency = Convert.ToInt32(Console.ReadLine());
-                }
-                catch {
-                    noValidAnswer = true;
-                }
-                if (desiredFrequency<0) {
-                    noValidAnswer = true;
-                }
-            }
-            return desiredFrequency;
-        }
 
         public static void Main() {
+            
 
-            Console.Write("Enter name of document to be read: ");
-            string chosenDocumentName = Console.ReadLine();
-            int desiredFrequency = GetUsersFrequency();
+            string chosenDocumentName = UserInput.GetUserDocumentName();
+            int desiredFrequency = UserInput.GetUsersFrequency();
 
             TlsDetector detector = new TlsDetector();
             detector.ReadDocument(chosenDocumentName);
